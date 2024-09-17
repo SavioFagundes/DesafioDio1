b7276dfa1ccdcfce261068e7cc9592ff9d1fbb5c
Savio Fagundes <akirasfm@gmail.com>
2024-09-17 19:48:50 -0300

    Adicionar arquivos de projeto.

---
+++ DesafioDio1/Entities/Estacionamento.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioDio1.Entities
+{
+    internal class Estacionamento
+    {
+        public decimal PrecoInicial { get; set; }
+        public decimal PrecoPorHora { get; set; }
+        private List<string> Veiculos = new List<string>();
+        public Estacionamento(decimal precoInicial, decimal precoPorHora)
+        {
+            PrecoInicial = precoInicial;
+            precoPorHora = precoPorHora;
+        }
+        public void AdicionarVeiculo()
+        {
+            Console.Write("Digite a placa do veículo para estacionar:");
+        }
+        public void RemoverVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo para remover:");
+            string placa = "";
+            if (Veiculos.Any(x => x.ToLower() == placa.ToLower()))
+            {
+                Console.Write("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+                int horas = 0;
+                decimal valorTotal = 0;
+                Console.Write($"O veículo {placa} foi tirado, e o preço total foi de: R$ {valorTotal}");
+            }
+            else
+            {
+                Console.WriteLine("Veículo não foi encontrado");
+            }
+        }
+        public void ListarVeiculos()
+        {
+            if (Veiculos.Any())
+            {
+                Console.Write("Os veículos estacionados são:");
+            }
+            else
+            {
+                Console.Write("Não há veículos estacionados.");
+            }
+        }
+    }
+}
---
+++ DesafioDio1/Program.cs
@@ -0,0 +1,59 @@
+﻿using DesafioDio1.Entities;
+using System.Collections.Generic;
+
+namespace Ex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal precoInicial = 0, precoPorHora = 0;
+            Console.WriteLine("Digite o preço inicial:");
+            precoInicial = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Agora digite o preço por hora:");
+            precoPorHora = decimal.Parse(Console.ReadLine());
+
+            Estacionamento estacionamentoGeral = new Estacionamento(precoInicial, precoPorHora);
+
+            bool exibirMenu = true;
+
+            while (exibirMenu)
+            {
+                Console.Clear();
+                Console.WriteLine("Digite a sua opção:");
+                Console.WriteLine("1 - Cadastrar veículo");
+                Console.WriteLine("2 - Remover veículo");
+                Console.WriteLine("3 - Listar veículos");
+                Console.WriteLine("4 - Encerrar");
+
+                switch (Console.ReadLine()) // ou elif >>
+                {
+                    case "1":
+                        estacionamentoGeral.AdicionarVeiculo();
+                        break;
+
+                    case "2":
+                        estacionamentoGeral.RemoverVeiculo();
+                        break;
+
+                    case "3":
+                        estacionamentoGeral.ListarVeiculos();
+                        break;
+
+                    case "4":
+                        exibirMenu = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção não contida");
+                        break;
+                }
+
+                Console.WriteLine("Pressione uma tecla para continuar");
+                Console.ReadLine();
+            }
+
+            Console.WriteLine("O programa se encerrou");
+        }
+    }
+}
